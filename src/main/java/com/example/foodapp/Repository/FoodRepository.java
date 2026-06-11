3de4a1aec05215f6e64c04a15b0834dc5b32f6ca
Kaviya Sri <kaviyasrinctit@gmail.com>
2026-06-11 15:08:40 +0530

    Update PostgreSQL connection

--- src/main/java/com/example/foodapp/Repository/FoodRepository.java
+++ src/main/java/com/example/foodapp/Repository/FoodRepository.java
@@ -3,9 +3,10 @@ package com.example.foodapp.Repository;
 
 import com.example.foodapp.Entity.FoodItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface FoodRepository extends JpaRepository<FoodItem, Long> {
 
     List<FoodItem> findByMoodType(String moodType);
